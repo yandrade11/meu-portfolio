498e952455309aec4356dbce2da9779b2f45ccf4
YURI ANDRADE <yuri.g.andrade@gmail.com>
2024-06-05 19:33:45 -0300

    feat: adicionando ant design no header para linkedin e github

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
 import "../Header/Header.scss";
 import { Link } from "react-router-dom";
+import { AiOutlineGithub, AiFillLinkedin  } from "react-icons/ai";
 
 export default function Header() {
   return (
     <header className="header">
       <ul>
+        <li className="menu-right">
+          <Link
+            to="https://www.linkedin.com/in/yuri-andrade-9292533a/"
+            className="icons"
+          >
+            <AiFillLinkedin  />
+          </Link>
+        </li>
+        <li className="menu-right">
+          <Link to="https://github.com/yandrade11" className="icons">
+            <AiOutlineGithub />
+          </Link>
+        </li>
         <li className="menu-right">
           <Link to="/contato">Contate-me</Link>
         </li>
