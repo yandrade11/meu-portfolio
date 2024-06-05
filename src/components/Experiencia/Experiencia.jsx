b30aac411d8a8b544809c0b1c8e73cecc4dbf4f0
YURI ANDRADE <yuri.g.andrade@gmail.com>
2024-06-05 19:09:40 -0300

    feat: criando componente experiencia

---
+++ src/components/Experiencia/Experiencia.jsx
@@ -0,0 +1,35 @@
+import "./Experiencia.scss";
+
+export default function Experiencia() {
+  return (
+    <>
+      <h3>Experiência</h3>
+
+      <p>
+        <b>MEDGrupo</b>, Desenvolvedor Frontend Jr.
+      </p>
+      <p>2020 - 2023</p>
+
+      <hr size="1"></hr>
+
+      <p>
+        <b>MEDGrupo</b>, Estagiário desenvolvimento Frontend Jr.
+      </p>
+      <p>2018 - 2020</p>
+
+      <hr size="1"></hr>
+
+      <p>
+        <b>Gotop</b>, Estagiário Marketing Digital e SEO
+      </p>
+      <p>2015 - 2016</p>
+
+      <hr size="1"></hr>
+
+      <p>
+        <b>Stefanini It Solutions</b>, Estagiário desenvolvimento .NET
+      </p>
+      <p>2011 - 2013</p>
+    </>
+  );
+}
