01fad78f0f3362a3c1bc5470196e28e9b47c4c5d
YURI ANDRADE <yuri.g.andrade@gmail.com>
2024-06-20 19:51:57 -0300

    fix: ajustando css da main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import "./components/DarkMode/palheta-cores.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
